bb1b6132d082c5cf9b08a2b26767cd34a1f45757
BSSE23086 <bsse23086@itu.edu.pk>
2026-06-29 20:23:19 +0500

    First deployment commit

---
+++ base/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# from django.db.models.deletion import CASCADE
+# Create your models here. databse models
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null = True)
+    email = models.EmailField(unique = True, null = True)
+    bio = models.TextField(null=True)
+    avatar = models.ImageField(null=True, default="avatar.svg")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    host =   models.ForeignKey(User, on_delete = models.SET_NULL, null=True)
+    topic =  models.ForeignKey(Topic, on_delete = models.SET_NULL, null=True)# allow it to be empty
+    name = models.CharField(max_length=200)
+# Make sure it matches this EXACTLY:
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
+    description = models.TextField(null = True, blank = True)
+    GENDER_CHOICES = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('None', 'None'),
+    ]
+    gender = models.CharField(
+        max_length=20, 
+        choices=GENDER_CHOICES, 
+        default='None',
+        null=True, 
+        blank=True
+    )    # participants =
+    updated = models.DateTimeField(auto_now = True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']# - to show new and updated first on the list 
+
+    def __str__(self):
+        return self.name
+class road(models.Model):
+    name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+    
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete = models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now = True)
+    created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['-updated', '-created']# - to show new and updated first on the list 
+
+    def __str__(self):
+        return self.body[0:50]
